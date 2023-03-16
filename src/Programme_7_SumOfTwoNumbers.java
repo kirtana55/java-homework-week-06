08b2d4c4b2233752afc81af2028d70529079c0cc
kirtana55 <kirtanapatell55@gmail.com>
2023-03-16 23:15:55 +0000

    My homework week 06

---
+++ src/Programme_7_SumOfTwoNumbers.java
@@ -0,0 +1,15 @@
+/**
+ * 7. Write a Java program to print the sum of the number.
+ * Test Data: 74 + 36
+ * Expected
+ */
+public class Programme_7_SumOfTwoNumbers {
+    public static void main(String[] args) {
+        //The two integers are value1 and value2
+        int value1 = 74;
+        int value2 = 36;
+        int sum = value1 + value2;
+        //concatenation
+        System.out.println("The Sum of " + value1 + " and " + value2 + " is " + sum);
+    }
+}
